f06fa642c1f15e07b0cb1b0f89eed2135c9e7899
Katja <arndt-katja@outlook.de>
2025-02-23 22:27:45 +0100

    Zeit in alert einfügen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 /*wenn select element ausgewählt wird */
-let cities = document.querySelector("#cities");
+/*let cities = document.querySelector("#cities");
 function selection() {
   let selectedCity = cities.value;
   alert(selectedCity);
 }
-let select = cities.addEventListener("change", selection);
+let select = cities.addEventListener("change", selection);*/
 
+let cities = document.querySelector("#cities");
+let selectedCity = cities.value;
+function selection() {
+  let newSelectedCity = cities.value;
+  let timeInNewSelectedCity = moment().format("dddd, MMMM D, YYYY, h:mm A");
+  alert("It is " + timeInNewSelectedCity + " in " + newSelectedCity);
+}
+
+let select = cities.addEventListener("change", selection);
 /*alert */
 
 /*timezone*/
