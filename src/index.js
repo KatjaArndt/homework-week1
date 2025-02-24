6dfe3b0ea9a4c1feafbe8ce69791e41bae7836a7
Katja <arndt-katja@outlook.de>
2025-02-24 20:04:40 +0100

    alert with timezone in selected city

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-/*wenn select element ausgewählt wird */
-/*let cities = document.querySelector("#cities");
-function selection() {
-  let selectedCity = cities.value;
-  alert(selectedCity);
-}
-let select = cities.addEventListener("change", selection);*/
-
 let cities = document.querySelector("#cities");
 let selectedCity = cities.value;
 function selection() {
   let newSelectedCity = cities.value;
-  let timeInNewSelectedCity = moment().format("dddd, MMMM D, YYYY, h:mm A");
-  alert("It is " + timeInNewSelectedCity + " in " + newSelectedCity);
+
+  let timeInTokyo = moment()
+    .tz("Asia/Tokyo")
+    .format("dddd, MMMM D, YYYY, h:mm A");
+  let timeInParis = moment()
+    .tz("Europe/Paris")
+    .format("dddd, MMMM D, YYYY, h:mm A");
+  let timeInSydney = moment()
+    .tz("Australia/Sydney")
+    .format("dddd, MMMM D, YYYY, h:mm A");
+  console.log(newSelectedCity);
+  if (newSelectedCity === "tokyo") {
+    alert("It is " + timeInTokyo + " in " + newSelectedCity);
+  } else if (newSelectedCity === "paris") {
+    alert("It is " + timeInParis + " in " + newSelectedCity);
+  } else if (newSelectedCity === "sydney") {
+    alert("It is " + timeInSydney + " in " + newSelectedCity);
+  }
 }
 
 let select = cities.addEventListener("change", selection);
-/*alert */
-
-/*timezone*/
